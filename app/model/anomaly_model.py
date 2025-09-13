d7c598a17cc7d0191baeeb423fca2cbfaf34b4ea
Jayson Pradhananga <jaysonyt2@gmail.com>
2025-09-13 21:37:21 +0545

    anomaly model push

--- app/model/anomaly_model.py
+++ app/model/anomaly_model.py
@@ -1,17 +1,20 @@
 from sklearn.ensemble import IsolationForest
 from sklearn.preprocessing import RobustScaler
 from sklearn.pipeline import Pipeline
+import joblib
 from preproccessing.preprocess_core import preprocessor
 
-def build_anomaly_model(df):
+def build_anomaly_model():
+    df = preprocessor()
+
     features = ["views", "likes", "comments", "duration"]
-    X = df[features]
+    X = df[features].copy()
     
     pipeline = Pipeline([
         ("scaler", RobustScaler()),
         ("isolation", IsolationForest(contamination=0.05, random_state=42))
     ])
 
-    df["is_anomaly"] = pipeline.fit_predict(features)
+    pipeline.fit(X)
 
-    return df
+    joblib.dump(pipeline, "saves/isolation_pipeline.pkl")
